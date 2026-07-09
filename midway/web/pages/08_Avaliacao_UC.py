eaa0225139216c556540f1e92a755e1a82b0d6b7
Tiago-lab-py <eng.tiagofurlan@gmail.com>
2026-07-09 06:58:05 -0300

    Inclui aba de reclamacoes na avaliacao de UC

--- midway/web/pages/08_Avaliacao_UC.py
+++ midway/web/pages/08_Avaliacao_UC.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from midway.web.library.avaliacao_uc import show_avaliacao_uc
+import streamlit as st
+
+from midway.web.library.avaliacao_uc import show_consulta_uc, show_outlier_uc
+from midway.web.library.reclamacoes_uc import show_reclamacoes_uc
 from midway.web.library.shared import configure_page, render_header, render_sidebar
 
 
@@ -8,4 +11,10 @@ configure_page("MIDWAY - Avaliação de UC")
 render_header("08 Avaliação de UC")
 _, db_path, sample_limit, _ = render_sidebar()
 
-show_avaliacao_uc(str(db_path), sample_limit)
+tabs = st.tabs(["Outlier UC", "Consulta UC", "Reclamações UC"])
+with tabs[0]:
+    show_outlier_uc(str(db_path), sample_limit)
+with tabs[1]:
+    show_consulta_uc(str(db_path), sample_limit)
+with tabs[2]:
+    show_reclamacoes_uc(str(db_path), sample_limit)
