a02501291258ea728ccb53d7d0f409e0d7b9d7db
Tiago-lab-py <eng.tiagofurlan@gmail.com>
2026-07-10 15:50:15 -0300

    Adiciona SQL de referencia componente causa IQS

---
+++ SQL/IQS_referencia_componente_causa.sql
@@ -0,0 +1,29 @@
+/*
+Referencia IQS para classificacao encadeada no Envio IQS.
+
+Fluxo:
+  1. Grupo de componente de rede
+  2. Componente do grupo
+  3. Causa permitida para o componente
+*/
+
+SELECT DISTINCT
+    gcr.COD_GRUPO_GCR,
+    gcr.DESC_GRUPO_GCR,
+    comp.COD_COMP,
+    comp.DESC_COMP,
+    ca.COD_CAUSA,
+    ca.DESC_CAUSA
+FROM sod.GRUPO_COMPONENTE_REDE gcr
+LEFT JOIN sod.COMPONENTE comp
+    ON gcr.PID = comp.PID_GCR_COMP
+LEFT JOIN sod.C_CAUSA cc
+    ON comp.PIDC_CAUSA_COMP = cc.PID
+LEFT JOIN sod.CAUSA ca
+    ON cc.PID_REF = ca.PID
+WHERE comp.COD_COMP IS NOT NULL
+  AND ca.COD_CAUSA IS NOT NULL
+ORDER BY
+    gcr.DESC_GRUPO_GCR,
+    comp.DESC_COMP,
+    ca.DESC_CAUSA
